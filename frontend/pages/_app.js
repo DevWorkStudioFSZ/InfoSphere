6b5fa14cd8f73a201eae8fb262d7a1c41df32772
humairabatoolbhatti <batoolhumaira37@gmail.com>
2025-10-02 22:59:31 +0500

    Delete frontend directory

--- frontend/pages/_app.js
+++
@@ -1,7 +0,0 @@
-import '../styles/globals.css'
-import 'leaflet/dist/leaflet.css'   // ✅ Add this
-
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
