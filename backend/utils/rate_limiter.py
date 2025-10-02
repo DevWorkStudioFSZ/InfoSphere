474c12a84fc22b2f5d84f3d12ce28fd918bae358
humairabatoolbhatti <batoolhumaira37@gmail.com>
2025-10-02 22:54:42 +0500

    Keep only frontend code in frontend branch

--- backend/utils/rate_limiter.py
+++
@@ -1,8 +0,0 @@
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
-
-# Global limiter instance
-limiter = Limiter(
-    key_func=get_remote_address,
-    default_limits=["100 per hour"]  # Default if route doesn’t override
-)
